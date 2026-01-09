cce1d8a126980a7a0c34b9ccfe48ddd381a956ee
Artem Podkolzin <temati26@gmail.com>
2026-01-10 05:15:53 +0900

    rotate cup

--- mug/mouse_spin.gd
+++ mug/mouse_spin.gd
@@ -1,8 +1,19 @@
 extends Node3D
 
 var active := false
-func _process(_delta):
-	if active:
-		var mouse_position := get_viewport().get_mouse_position()
-		print(mouse_position)
-		rotation.y = mouse_position.x / 400
+var rotation_speed := 0.085 # start animation
+var previous_mouse_position := Vector2.ZERO
+
+func _process(delta: float):
+	if Input.is_action_just_pressed("left_click"):
+		# Start tracking
+		previous_mouse_position = get_viewport().get_mouse_position()
+	elif Input.is_action_pressed("left_click"):
+		# Difference since last frame -> speed
+		var mouse_position := get_viewport().get_mouse_position() 
+		rotation_speed = (mouse_position.x - previous_mouse_position.x) / 400
+		previous_mouse_position = mouse_position
+	if rotation_speed:
+		# Rotate and damp speed
+		rotation.y += rotation_speed
+		rotation_speed = lerpf(rotation_speed, 0.0, 1.5 * delta)
