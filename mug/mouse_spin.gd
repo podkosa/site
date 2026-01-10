174c51de58f8123543cd7bbab38ff5e848f2fc98
Artem Podkolzin <temati26@gmail.com>
2026-01-10 18:03:17 +0900

    tweak animation

--- mug/mouse_spin.gd
+++ mug/mouse_spin.gd
@@ -1,10 +1,14 @@
 extends Node3D
 
 var active := false
-var rotation_speed := 0.085 # start animation
+var rotation_speed := 0.0 # start animation
 var previous_mouse_position := Vector2.ZERO
+@onready var initial_animation: AnimationPlayer = $AnimationPlayer
 
-func _process(delta: float):
+func _ready() -> void:
+	scale = Vector3.ZERO
+
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("left_click"):
 		# Start tracking
 		previous_mouse_position = get_viewport().get_mouse_position()
@@ -17,3 +21,10 @@ func _process(delta: float):
 		# Rotate and damp speed
 		rotation.y += rotation_speed
 		rotation_speed = lerpf(rotation_speed, 0.0, 1.5 * delta)
+
+
+
+func _on_timer_timeout() -> void:
+	initial_animation.animation_set_next("initial", "float")
+	initial_animation.play("initial")
+	rotation_speed = 0.095
